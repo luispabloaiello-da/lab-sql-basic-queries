56d250edf2355e726464ce96fb2032ad0c2e8346
luispabloaiello-da <luispablo.aiello@gmail.com>
2025-09-17 23:16:01 +0200

    Solved lab

--- lab-sql-basic-queries.sql
+++ lab-sql-basic-queries.sql
@@ -33,6 +33,9 @@ select distinct release_year as year from sakila.film;
 -- 5.1 Determine the number of stores that the company has.
 select count(store_id) from sakila.store;
 
+-- 5.2 Determine the number of employees that the company has.
+select count(*) from sakila.staff;
+
 -- 5.3 Determine how many films are available for rent and how many have been rented.
 select count(distinct(inventory_id)) AS Rented, ((select count(film_id) from sakila.inventory) - count(distinct(inventory_id))) as Available from sakila.rental;
 
